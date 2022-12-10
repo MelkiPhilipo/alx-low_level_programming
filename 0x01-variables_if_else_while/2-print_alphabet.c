dd408b7f4aeb99924d8aee9356834c5e84b8ba62
MelkiPhilipo <melkiphilipo07@gmail.com>
2022-12-10 09:36:26 +0300

    B

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- *  main - Entry point
- *
- *  Return: Always 0 (Success)
- */
-
+*  main - Entry point
+*
+*  Return: Always 0 (Success)
+*/
 int main(void)
 {
 	char low;
@@ -15,6 +13,5 @@ int main(void)
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 		putchar('\n');
-
 	return (0);
 }
