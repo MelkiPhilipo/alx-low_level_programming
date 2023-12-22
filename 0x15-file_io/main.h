6f7f95a70098ea795774e371bfd868c1f52cc45d
MelkiPhilipo <melkiphilipo07@gmail.com>
2023-12-22 17:04:33 +0300

    file io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/fcntl.h>
+#include <string.h>
+#include <error.h>
 
 int main(int ac, char **av);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
